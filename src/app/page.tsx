d2b620b80b574556f3c89521ba6d46c74fc4bf80
Otavio Dias <otaviodias@gmail.com>
2026-03-06 01:41:15 +0000

    update title & description layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { api } from "~/trpc/server";
+
 export default async function Home() {
-  return <h1>Funcionando!!!</h1>;
+  const matches = await api.db.matchesCards();
+  return matches.map((match) => (
+    <h1 key={match.id} className="uppercase">
+      {match.is_home ? "Flamengo" : match.opponent}{" "}
+      {match.scoreboard?.home ?? ""} - {match.scoreboard?.away ?? ""}{" "}
+      {match.is_home ? match.opponent : "Flamengo"}
+    </h1>
+  ));
 }
