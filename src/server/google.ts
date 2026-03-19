7f46fe949377d727f2491b00610e383399ea593d
Otavio Dias <otaviodias@gmail.com>
2026-03-19 20:31:41 +0000

    fix redirect_uri

--- src/server/google.ts
+++ src/server/google.ts
@@ -1,8 +1,13 @@
 import { google } from "googleapis";
 import { env } from "~/env.js";
 
+const REDIRECT_URI =
+  process.env.NODE_ENV === "production"
+    ? "https://agenda-flamengo.vercel.app/api/callback"
+    : "http://localhost:3000/api/callback";
+
 export const oauth2Client = new google.auth.OAuth2(
   env.GOOGLE_CLIENT_ID,
   env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:3000/api/callback",
+  REDIRECT_URI,
 );
